2374857370e2798b748aa418f046eb62c8683252
Victor Lira <vlmds02@gmail.com>
2022-12-17 15:08:07 -0400

    FlappyBird3D ended

---
+++ Assets/Obstaculo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -5.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.z < -30f)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
